9ef61a2837d3c9710b034965b3b3437cce3c1666
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-06-17 18:30:15 -0300

    Custom bind

---
+++ spring-cloud-producer-custom/src/main/java/com/example/RabbitMQ/springcloudproducercustom/SpringCloudProducerCustomApplication.java
@@ -0,0 +1,13 @@
+package com.example.RabbitMQ.springcloudproducercustom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudProducerCustomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudProducerCustomApplication.class, args);
+	}
+
+}
